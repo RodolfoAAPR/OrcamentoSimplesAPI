28f1b619a3c8e8ca1b86d66da33ed91acb581cde
RodolfoAAPR <165041838+RodolfoAAPR@users.noreply.github.com>
2025-11-03 13:49:24 -0300

    feat(CustomerService): add create, update, delete and find on service

---
+++ src/main/java/com/orcamentomecanico/OrcamentoSimples/service/CustomerService.java
@@ -0,0 +1,39 @@
+package com.orcamentomecanico.OrcamentoSimples.service;
+
+import com.orcamentomecanico.OrcamentoSimples.model.CustomerModel;
+import com.orcamentomecanico.OrcamentoSimples.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<CustomerModel> findAllCustomers(){
+        return customerRepository.findAll();
+    }
+
+    public CustomerModel registerCustomer(CustomerModel customerModel){
+        return customerRepository.save(customerModel);
+    }
+
+    public void deleteCustomer(Long id){
+        customerRepository.deleteById(id);
+    }
+
+    public CustomerModel updateCustomer(Long id, CustomerModel customerModel){
+        CustomerModel newCustomer = customerRepository.findById(id).get();
+        newCustomer.setCpf(customerModel.getCpf());
+        newCustomer.setName(customerModel.getName());
+        newCustomer.setEmail(customerModel.getEmail());
+        newCustomer.setNumber(customerModel.getNumber());
+        return customerRepository.save(newCustomer);
+    }
+
+
+
+}
