74cbdce0e8282e03118f5c956342e3100e8c87be
RodolfoAAPR <165041838+RodolfoAAPR@users.noreply.github.com>
2025-11-03 12:00:41 -0300

    feat(app): start spring boot project

---
+++ src/main/java/com/orcamentomecanico/OrcamentoSimples/OrcamentoSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.orcamentomecanico.OrcamentoSimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoSimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orcamentomecanico/OrcamentoSimples/OrcamentoSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orcamentomecanico.OrcamentoSimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
